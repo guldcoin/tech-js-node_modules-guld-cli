bea1f6af32d1d16fbb659d056fbb02c17d9d0153
Ira Miller <public@iramiller.com>
2018-06-30 19:46:08 -0500

    all commands

--- cli.js
+++ cli.js
@@ -5,15 +5,20 @@ const VERSION = pkg.version
 const NAME = Object.keys(pkg.bin)[0]
 
 const COMMANDS = {
-  'config': ['Manage git config files the guld way.', {isDefault: true}],
-  'env': ['Guld environment detection module.'],
-  'git': ['Guld standardized Command Line Interface (CLI) for git.']
+  'user': ['Guld user management tools. Get, list, and check users of the guld group.', {isDefault: true}],
+  'env': ['Guld environment detection.'],
+  'git': ['Git tools and commands.'],
+  'mail': ['Guld mail is a signed, encrypted, and witnessed email system.'],
+  'keys': ['Guld key management.'],
+  'pass': ['Encrypted password management.'],
+  'ledger': ['Guld ledger.'],
+  'fs': ['Guld filesystem tools.']
 }
 
 program
   .name('guld')
   .version(VERSION)
-  .description('Guld decentralized internet CLI.')
+  .description('Guld decentralized internet command line.')
   .option('-u, --user', 'The user name to set up.')
   .option('-r, --recipient', 'The recipient of a message or transaction.')
   .option('-f, --fingerprint', 'The PGP fingerprint to sign with.')
@@ -23,9 +28,9 @@ for (var cmd in COMMANDS) {
   var cmds = COMMANDS[cmd]
   var desc = cmds.shift()
   if (cmds.length > 0) {
-    program.command(cmd, desc, ...cmds).description(desc)
+    program.command(cmd, desc, ...cmds)
   } else {
-    program.command(cmd, desc).description(desc)
+    program.command(cmd, desc)
   }
 }
 
